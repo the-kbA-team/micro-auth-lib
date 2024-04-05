2cd7dce8d45f5d589a704c89862c9e40dfc4a081
Gregor <gregor-j@users.noreply.github.com>
2024-04-05 19:09:02 +0200

    PHP Code Beautifier auto-run #13

--- src/Traits/ParamChecksum.php
+++ src/Traits/ParamChecksum.php
@@ -24,7 +24,7 @@ trait ParamChecksum
         $checksum = trim((string)filter_var(
             $input[self::CHECKSUM],
             FILTER_SANITIZE_STRING,
-            FILTER_FLAG_STRIP_LOW|FILTER_FLAG_STRIP_HIGH|FILTER_FLAG_STRIP_BACKTICK
+            FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_BACKTICK
         ));
         if (empty($checksum)) {
             throw new InvalidParameterException('Checksum is empty.');
